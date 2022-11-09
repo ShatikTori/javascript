3f99934cdb223cb93311f6e7492b834d531249ac
ShatikTori <tori167@yandex.ru>
2022-11-09 16:06:22 +0100

    Update script.js

--- Domashna 1 Calculator/script.js
+++ Domashna 1 Calculator/script.js
@@ -1,9 +1,12 @@
 const number1 = 10
-const number2 = 3
-let operation = '%'
+const number2 = 2
+let operation = '/'
 let result
 
 
+if (number2 === 0) {
+    console.log("Cannot divide by zero")
+} else if (number2 > 0) {
 switch (operation) {
     case '+': {
         result = number1 + number2
@@ -32,5 +35,5 @@ switch (operation) {
     case '%': {
         result = number1 % number2
         console.log("Остатокот од поделбата на првиот број со вториот е " + result)
-    }
-  }
+    } 
+  }}
